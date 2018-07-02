cf14be143f0d54f6371076ca06a694744ab7f2ff
Michael Snoyman <michael@snoyman.com>
2018-07-03 00:41:29 +0300

    WIP Initial work on #3922
    This is nowhere near ready, it just paints a basic idea of a direction
    to start in. This is based on a whiteboard drawing I did, which may or
    may not be helpful.
    
    https://imgur.com/a/1jv3WLp

---
+++ src/Stack/Types/SourceMap.hs
@@ -0,0 +1,80 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Stack.Types.SourceMap
+  ( -- * Unresolved
+    UnresolvedSourceMap
+  , UnresolvedPackageSource (..)
+  , UnresolvedInstallable (..)
+    -- * Loaded
+  , LoadedInstallable (..)
+    -- * Resolved
+  , ResolvedSourceMap
+  , RPSKey
+  , ResolvedPackageSourceKey (..)
+  , ResolvedPackageSource (..)
+  , ResolvedInstallable (..)
+  , Toolchain (..)
+  , mkRPSKey
+  ) where
+
+import Distribution.PackageDescription (TestSuiteInterface)
+import Stack.Prelude
+import Stack.Types.BuildPlan
+import Stack.Types.GhcPkgId
+import Stack.Types.Package (PackageConfig, PackageLibraries)
+import Stack.Types.PackageIdentifier
+import Stack.Types.PackageName
+import Stack.Types.Version
+
+type UnresolvedSourceMap = Map PackageName UnresolvedPackageSource
+
+data UnresolvedPackageSource
+  = UPSGlobal !Version !GhcPkgId
+  | UPSInstallable !UnresolvedInstallable
+
+data UnresolvedInstallable = UnresolvedInstallable
+  { uiLocation :: !(PackageLocationIndex FilePath)
+  , uiPackageConfig :: !PackageConfig -- FIXME probably don't need GHC info in here directly
+  }
+
+data LoadedInstallable = LoadedInstallable
+  { liUnresolved :: !UnresolvedInstallable
+  , liLibraryDeps :: !(Map PackageName VersionRange)
+  -- ^ After accounting for flags and what components are enabled,
+  -- this is all library dependencies we need, including setup
+  -- dependencies. They are also tracked separately in 'liSetupDeps'
+  -- for properly building the @Setup.hs@ files.
+  , liToolDeps :: !(Map ExeName VersionRange) -- FIXME deal better with Cabal 2.0 stuff?
+  , liVersion :: !Version
+  , liLibraries :: !PackageLibraries
+  , liInternalLibraries :: !(Set Text)
+  , liTests :: !(Map Text TestSuiteInterface)
+  , liBenchmarks :: !(Set Text)
+  , liExes :: !(Set ExeName)
+  , liSetupDeps :: !(Maybe (Map PackageName VersionRange))
+  }
+
+type ResolvedSourceMap = Map PackageName ResolvedPackageSourceKey
+
+newtype RPSKey = RPSKey { _unRPSKey :: StaticSHA256 }
+
+data ResolvedPackageSourceKey = RPSK !ResolvedPackageSource !RPSKey
+
+data ResolvedPackageSource
+  = RPSGlobal !Version !GhcPkgId
+  | RPSInstallable !ResolvedInstallable
+
+data ResolvedInstallable = ResolvedInstallable
+  { riUnresolved :: !UnresolvedInstallable
+  , riDeps :: !(Map PackageName RPSKey)
+  }
+
+data Toolchain = Toolchain
+  -- FIXME identify GHC version, install location, arch, whatever else
+  -- we can think of
+
+mkRPSKey
+  :: Toolchain
+  -> PackageName
+  -> ResolvedPackageSource
+  -> RPSKey
+mkRPSKey _ _ _ = RPSKey $ error "mkRPSKey"
